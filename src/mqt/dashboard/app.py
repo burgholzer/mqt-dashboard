d4f0776b2000204e80e1034b501d1432764c259c
burgholzer <burgholzer@me.com>
2025-12-30 19:14:07 +0100

    🏷️ proper flask typing
    Signed-off-by: burgholzer <burgholzer@me.com>

--- src/mqt/dashboard/app.py
+++ src/mqt/dashboard/app.py
@@ -9,11 +9,11 @@ from .visualization import create_summary_cards
 app = Flask(__name__, template_folder="templates")
 
 
-@app.route("/")  # type: ignore[misc]
+@app.route("/")
 def index() -> str:
     """Return the index page with summary cards."""
     sorted_by_stars, sorted_by_downloads, total_stars, total_downloads = create_summary_cards()
-    return render_template(  # type: ignore[no-any-return]
+    return render_template(
         "index.html",
         sorted_by_stars=sorted_by_stars,
         sorted_by_downloads=sorted_by_downloads,
